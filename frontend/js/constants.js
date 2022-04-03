c65c16891413c8d5ee80f42ba672bae60394460e
SolDogNFTs <Eidantonio58@gmail.com>
2022-04-03 20:54:35 +0300

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,9 +1,9 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x4B24E0cDB235a8D81C3DDE4a4A5e73Eb0670bAF4"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Welcome to the SolDogNFTs Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_p = 'SolDogNFTs is a collection containing 500 Unique animated dog pictures , the holders of these unique NFTs will be invited to some exclusive events that will be hosted on our official discord server , the event will contain tons off giveaways and some special NFTs drops !.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
